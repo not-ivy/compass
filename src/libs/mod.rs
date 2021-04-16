1bc7865fd7b897c7e637354875135b264e47c55e
sourtaste000 <sourtaste000@gmail.com>
2021-04-15 22:42:09 -0700

    format

--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -4,28 +4,77 @@ use crate::libs::structs::{Author, Config, Embed, Field, Footer, Image, Message,
 use std::fs;
 
 impl Message {
-    pub fn new(username: &str, avatar_url: &str, content: &str, embeds: Option<Vec<Embed>>) -> Message{
+    pub fn new(
+        username: &str,
+        avatar_url: &str,
+        content: &str,
+        embeds: Option<Vec<Embed>>,
+    ) -> Message {
         Message {
             username: username.to_string(),
             avatar_url: avatar_url.to_string(),
             content: content.to_string(),
-            embeds: match embeds {Some(embeds) => embeds, None => vec![]}
+            embeds: match embeds {
+                Some(embeds) => embeds,
+                None => vec![],
+            },
         }
     }
 }
 
 impl Embed {
-    pub fn new(author: Option<Author>, title: &str, url: Option<&str>, description: Option<&str>, color: Option<u64>, fields: Option<Vec<Field>>, thumbnail_url: Option<&str>, image_url: Option<&str>, footer: Option<Footer>) -> Embed {
+    pub fn new(
+        author: Option<Author>,
+        title: &str,
+        url: Option<&str>,
+        description: Option<&str>,
+        color: Option<u64>,
+        fields: Option<Vec<Field>>,
+        thumbnail_url: Option<&str>,
+        image_url: Option<&str>,
+        footer: Option<Footer>,
+    ) -> Embed {
         Embed {
-            author: match author {Some(author) => author, None => Author::new("", None, None)},
+            author: match author {
+                Some(author) => author,
+                None => Author::new("", None, None),
+            },
             title: title.to_string(),
-            url: match url {Some(url) => url.to_string(), None => String::from("")},
-            description: match description {Some(description) => description.to_string(), None => String::from("")},
-            color: match color {Some(color) => color, None => 0 as u64},
-            fields: match fields {Some(field) => field, None => vec![]},
-            thumbnail: Thumbnail { url: match thumbnail_url {Some(thumbnail_url) => thumbnail_url.to_string(), None => String::from("")} },
-            image: Image { url: match image_url {Some(image_url) => image_url.to_string(), None => String::from("")}},
-            footer: match footer {Some(footer) => footer, None => Footer{text: String::from(""), icon_url: String::from("")}}
+            url: match url {
+                Some(url) => url.to_string(),
+                None => String::from(""),
+            },
+            description: match description {
+                Some(description) => description.to_string(),
+                None => String::from(""),
+            },
+            color: match color {
+                Some(color) => color,
+                None => 0 as u64,
+            },
+            fields: match fields {
+                Some(field) => field,
+                None => vec![],
+            },
+            thumbnail: Thumbnail {
+                url: match thumbnail_url {
+                    Some(thumbnail_url) => thumbnail_url.to_string(),
+                    None => String::from(""),
+                },
+            },
+            image: Image {
+                url: match image_url {
+                    Some(image_url) => image_url.to_string(),
+                    None => String::from(""),
+                },
+            },
+            footer: match footer {
+                Some(footer) => footer,
+                None => Footer {
+                    text: String::from(""),
+                    icon_url: String::from(""),
+                },
+            },
         }
     }
 }
@@ -35,7 +84,7 @@ impl Field {
         Field {
             name: name.to_string(),
             value: value.to_string(),
-            inline
+            inline,
         }
     }
 }
@@ -44,17 +93,26 @@ impl Author {
     pub fn new(name: &str, url: Option<&str>, icon_url: Option<&str>) -> Author {
         Author {
             name: name.to_string(),
-            url: match url { Some(url) => url.to_string(), None => String::from("") },
-            icon_url: match icon_url { Some(icon_url) => icon_url.to_string(), None => String::from("")}
+            url: match url {
+                Some(url) => url.to_string(),
+                None => String::from(""),
+            },
+            icon_url: match icon_url {
+                Some(icon_url) => icon_url.to_string(),
+                None => String::from(""),
+            },
         }
     }
 }
 
 impl Footer {
-    pub fn new(text: &str, icon_url: Option<&str>) -> Footer{
+    pub fn new(text: &str, icon_url: Option<&str>) -> Footer {
         Footer {
             text: text.to_string(),
-            icon_url: match icon_url {Some(icon_url) => icon_url.to_string(), None => String::from("")}
+            icon_url: match icon_url {
+                Some(icon_url) => icon_url.to_string(),
+                None => String::from(""),
+            },
         }
     }
 }
